0f27149c0085272cad3b3694ca3f311dca94ebfe
Brad Lewis <BLewis739@gmail.com>
2022-04-15 11:31:16 -0400

    User created

---
+++ migrations/20220415152627-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      zipCode: {
+        type: Sequelize.INTEGER
+      },
+      scores: {
+        type: Sequelize.ARRAY
+      },
+      aggregateScore: {
+        type: Sequelize.NUMBER
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
