ba6ca9c5707f85250ac7487204d311cfe14ee29e
milywendland <milynicwendland@gmail.com>
2022-04-18 07:33:53 -0600

    table fix

--- migrations/20220418120858-create-user-scores.js
+++ migrations/20220418120858-create-user-scores.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserScores', {
+    await queryInterface.createTable('user_score_list', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -19,9 +19,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserScores');
+    await queryInterface.dropTable('user_score_list')
   }
-};
+}
